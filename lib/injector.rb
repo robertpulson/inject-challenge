2694f6b2fe6468def70f28c154efc4097cc8f91e
Phil Brockwell <nineteen40eight@gmail.com>
2015-02-15 20:32:05 +0000

    all tests passing, refactoring needed

--- lib/injector.rb
+++ lib/injector.rb
@@ -1,29 +1,43 @@
 class Array
 
   def copy
-      self.dup
+    dup
   end
 
-      # def injector(&block)
-      #   copiedArray = copy
-      #   a = copiedArray.shift
-      #     copiedArray.each do |b|
-      #       a = block.call(a, b)
-      #     end
-      #   a
-      # end  
-
-      # def injector(start=nil, symbol)
-      #   output = start 
-      #   output = copy[0] if start == nil 
-      #   i = 0
-      #   i = 1 if start == nil
-        
-      #   while i < copy.length
-      #     output = output.send(symbol, copy[i])
-      #     i = i + 1
-      #   end 
-      #   output
-      # end
+  def injector(*args)
+    if args.length == 2
+      start, symbol = args
+    elsif args.length == 1
+      symbol = args.first if args.first.is_a?(Symbol)
+      start = args.first if args.first.is_a?(Fixnum)
+    else
+      raise ArgumentError unless block_given?
+    end
+    
+    symbol = args[1] if args.length > 1
+    if block_given?
+      copiedArray = copy
+      if start
+        a = start
+      else
+        a = copiedArray.shift
+      end
+      copiedArray.each do |b|
+        a = yield(a, b)
+      end
+      a
+    else
+      output = copy[0]
+      output = start if start
+      i = 0
+      i = 1 unless start
+      
+      while i < copy.length
+        output = output.send(symbol.to_proc, copy[i])
+        i = i + 1
+      end 
+      output
+    end
+  end
 
 end
--- spec/injector_spec.rb
+++ spec/injector_spec.rb
@@ -1,40 +1,46 @@
-require "injector"
+require 'injector'
 
 describe Array do
 
-  let (:array) {Array.new}
+  let (:array) { Array.new }
 
   def fill_array
     array = [1, 2, 3]
   end
 
-  it "can make a copy of the array" do
+  it 'can make a copy of the array' do
     fill_array
     expect(array.copy).to eq(array)
   end
 
-  it "it can receive a '+'' and return a sum of the array" do
-    array = [1, 2, 3]
-    expect(array.injector(:+)).to eq(6)
+  it "it can receive a '+' and return the correct result" do
+    fill_array
+    expect(array.injector(:+)).to eq(array.inject(:+))
   end
 
   it "can receive a '-' and return the correct result" do
-    array = [1, 2, 3]
-    expect(array.injector(:-)).to eq(-4)
+    fill_array
+    expect(array.injector(:-)).to eq(array.inject(:-))
   end
 
-  it "returns an error if no arguement is passed" do
+  it 'returns an error if no arguement is passed' do
+    fill_array
     expect{ array.injector }.to raise_error(ArgumentError)
   end
 
-  it "can receive two args, first = start, second = symbol" do
-    array = [1, 2, 3]
-    expect(array.injector(5, :+)).to eq(11)
+  it 'can receive two args, first = start, second = symbol' do
+    fill_array
+    expect(array.injector(5, :+)).to eq(array.inject(5, :-))
   end
 
-  it "can receive a block and return same result as inject" do
-    array = [1, 2, 3]
+  it 'can receive a block and return same result as inject' do
+    fill_array
     expect(array.injector{ |sum, num| sum / num }).to eq(array.inject{ |sum, num| sum / num })
+  end
+
+  it 'can receive a block with an initial value and return the same result as inject' do
+    fill_array
+    expect(array.injector(1){ |sum, num| sum / num }).to eq(array.inject(1){ |sum, num| sum / num })
   end
 
 end
