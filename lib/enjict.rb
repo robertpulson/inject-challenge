07820761c4486b4981bdd204f874157087294548
Phil Brockwell <nineteen40eight@gmail.com>
2015-04-27 00:56:52 +0100

    added readme

--- lib/enjict.rb
+++ lib/enjict.rb
@@ -1,33 +1,25 @@
+# Open array class to add 'enjict' method
 class Array
-
   def enjict(*args)
-
     if args.length == 2 && args[0].is_a?(Fixnum) && args[1].is_a?(Symbol)
       start, symbol = args
     elsif args.length == 1
-      raise ArgumentError unless args.first.is_a?(Symbol) || args.first.is_a?(Fixnum)
+      fail ArgumentError unless args.first.is_a?(Symbol) || args.first.is_a?(Fixnum)
       symbol = args.first if args.first.is_a?(Symbol)
       start = args.first if args.first.is_a?(Fixnum)
     else
-      raise ArgumentError unless block_given?
+      fail ArgumentError unless block_given?
     end
-    
-    copiedArray = dup
 
-    start = copiedArray.shift unless start
-    
+    copied_array = dup
+
+    start = copied_array.shift unless start
+
     if block_given?
-      copiedArray.each { |num| start = yield(start, num) }
+      copied_array.each { |num| start = yield(start, num)       }
     else
-      copiedArray.each { |num| start = start.send(symbol, num) }
+      copied_array.each { |num| start = start.send(symbol, num) }
     end
     start
   end
 end
-
-
-#array = [1, 2, 3]
-#array.inject(:+) = 6
-#array.inject{ |output, num| output + num } = 6
-#array.inject(0, :+) = 6
-#array.inject(0) { |output, num| output + num } = 6
--- spec/enjict_spec.rb
+++ spec/enjict_spec.rb
@@ -1,15 +1,14 @@
 require 'enjict'
 
 describe Array do
-
-  let (:array) { [1, 2, 3] }
+  let(:array) { [1, 2, 3] }
 
   it "it can receive a '+' and return the correct result" do
-    expect(array.enjict(:+)).to eq(6)
+    expect(array.enjict(:+)).to eq(array.inject(:+))
   end
 
   it "can receive a '-' and return the correct result" do
-    expect(array.enjict(:-)).to eq(-4)
+    expect(array.enjict(:-)).to eq(array.inject(:-))
   end
 
   it 'can receive an arg and a symbol and return the correct result' do
@@ -17,7 +16,7 @@ describe Array do
   end
 
   it 'returns an error if no arguement is passed' do
-    expect{ array.enjict }.to raise_error(ArgumentError)
+    expect { array.enjict }.to raise_error(ArgumentError)
   end
 
   it 'can receive two args, first = start, second = symbol' do
@@ -25,23 +24,22 @@ describe Array do
   end
 
   it 'can receive a block and return same result as inject' do
-    expect(array.enjict{ |sum, num| sum / num }).to eq(array.inject{ |sum, num| sum / num })
+    expect(array.enjict { |a, e| a / e }).to eq(array.inject { |a, e| a / e })
   end
 
-  it 'can receive a block with an initial value and return the same result as inject' do
-    expect(array.enjict(1){ |sum, num| sum / num }).to eq(array.inject(1){ |sum, num| sum / num })
+  it 'can receive a block and initial value and return the same result as inject' do
+    expect(array.enjict(1) { |a, e| a / e }).to eq(array.inject(1) { |a, e| a / e })
   end
 
   it 'will return an error if two incorrect args are passed' do
-    expect{ array.enjict(1,1) }.to raise_error(ArgumentError)
+    expect { array.enjict(1, 1) }.to raise_error(ArgumentError)
   end
 
   it 'will return an error if one incorrect arg is passed on its own' do
-    expect{ array.enjict("string") }.to raise_error(ArgumentError)
+    expect { array.enjict('string') }.to raise_error(ArgumentError)
   end
 
   it 'will return an error if three args are passed' do
-    expect{ array.enjict(0, 1, :+) }.to raise_error(ArgumentError)
+    expect { array.enjict(0, 1, :+) }.to raise_error(ArgumentError)
   end
-
 end
